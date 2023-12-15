af7ad562057b5e656f26b799640603aa009edce4
Marc Rufeis <github@craftycram.net>
2023-12-15 21:03:41 +0100

    format

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,118 +1,179 @@
 <script setup>
-import { ref, watch } from 'vue'
+import { ref, watch } from 'vue';
 
-const wordlist = ref([])
-const loaded = ref(false)
-const password = ref('')
-const customize = ref(false)
-const length = ref(4)
-const digit = ref(true)
-const keepCase = ref(true)
-const uppercase = ref(true)
-const dash = ref(true)
+const wordlist = ref([]);
+const loaded = ref(false);
+const password = ref('');
+const customize = ref(false);
+const length = ref(4);
+const digit = ref(true);
+const keepCase = ref(true);
+const uppercase = ref(true);
+const dash = ref(true);
 
-const copied = ref(false)
-const copiedTimeout = ref(null)
+const copied = ref(false);
+const copiedTimeout = ref(null);
 
 fetch('/wordlist.txt')
-  .then(response => response.text())
-  .then(text => text.split('\n'))
-  .then(list => wordlist.value = list)
-  .then(() => loaded.value = true)
-  .then(() => generate())
+  .then((response) => response.text())
+  .then((text) => text.split('\n'))
+  .then((list) => (wordlist.value = list))
+  .then(() => (loaded.value = true))
+  .then(() => generate());
 
 function generate() {
   console.log(wordlist);
-  copied.value = false
+  copied.value = false;
 
-  let words = []
+  let words = [];
   for (let i = 0; i < length.value; i++) {
-    const randomIndex = Math.floor(Math.random() * wordlist.value.length)
-    words.push(wordlist.value[randomIndex])
+    const randomIndex = Math.floor(Math.random() * wordlist.value.length);
+    words.push(wordlist.value[randomIndex]);
   }
 
-  let result = ''
+  let result = '';
   if (uppercase.value && !keepCase.value) {
-    words = words.map(word => {
+    words = words.map((word) => {
       console.log(word);
-      return `${word.charAt(0).toUpperCase()}${word.slice(1)}`
-    })
+      return `${word.charAt(0).toUpperCase()}${word.slice(1)}`;
+    });
   } else if (!keepCase.value) {
-    words = words.map(word => word.toLowerCase())
+    words = words.map((word) => word.toLowerCase());
   }
   if (digit.value) {
-    const randomIndex = Math.floor(Math.random() * 10)
-    words.push(randomIndex)
+    const randomIndex = Math.floor(Math.random() * 10);
+    words.push(randomIndex);
   }
   if (dash.value) {
-    result = words.join('-')
+    result = words.join('-');
   } else {
-    result = words.join('')
+    result = words.join('');
   }
 
-  password.value = result
+  password.value = result;
 }
 
 async function copy() {
   try {
     await navigator.clipboard.writeText(password.value);
-    copied.value = true
-    clearTimeout(copiedTimeout.value)
+    copied.value = true;
+    clearTimeout(copiedTimeout.value);
     copiedTimeout.value = setTimeout(() => {
-      copied.value = false
-    }, 1000)
-  } catch($e) {
-    copied.value = false
+      copied.value = false;
+    }, 1000);
+  } catch ($e) {
+    copied.value = false;
   }
 }
 
-watch(length, (update) => length.value = update > 64 ? 64 : update <= 0 ? 1 : update)
+watch(
+  length,
+  (update) => (length.value = update > 64 ? 64 : update <= 0 ? 1 : update),
+);
 </script>
 
 <template>
   <main class="container">
-    <div class="col center" style="align-items: start;">
+    <div class="col center" style="align-items: start">
       <h1 class="mt0">r1cht1g pf3rd</h1>
       <div style="width: 100%" class="row">
         <div class="col">
-          <input type="text" readonly v-model="password" style="text-transform: none; width: 80%;">
+          <input
+            type="text"
+            readonly
+            v-model="password"
+            style="text-transform: none; width: 80%"
+          />
         </div>
         <div class="col">
-          <input class="ml1 words" type="number" name="length" id="length" v-model="length" min="1" max="64" style="width: 7ch" @change="generate">
+          <input
+            class="ml1 words"
+            type="number"
+            name="length"
+            id="length"
+            v-model="length"
+            min="1"
+            max="64"
+            style="width: 7ch"
+            @change="generate"
+          />
           <!-- label for="length" class="ml1">length</-->
         </div>
       </div>
       <div class="row w100">
-        <button @click="generate" :disabled="!loaded" class="mtb1 mr1 primary">Generate</button>
-        <button @click="copy" :disabled="!loaded" class="mtb1 mr1 primary" style="width: calc(4ch + 40px);">{{ copied ? '✔︎' : 'Copy' }}</button>
+        <button @click="generate" :disabled="!loaded" class="mtb1 mr1 primary">
+          Generate
+        </button>
+        <button
+          @click="copy"
+          :disabled="!loaded"
+          class="mtb1 mr1 primary"
+          style="width: calc(4ch + 40px)"
+        >
+          {{ copied ? '✔︎' : 'Copy' }}
+        </button>
         <!--div class="row mtb1" style="align-items: center;">
           <input type="number" name="length" id="length" v-model="length" min="1" max="64" style="width: 7ch" @change="generate">
           <label for="length" class="ml1">length</label>
         </div-->
-        <div class="ml1" style="display: inline-flex;">
-          <div class="row" style="align-items: center;">
-            <input type="checkbox" name="customize" id="customize" v-model="customize">
+        <div class="ml1" style="display: inline-flex">
+          <div class="row" style="align-items: center">
+            <input
+              type="checkbox"
+              name="customize"
+              id="customize"
+              v-model="customize"
+            />
             <label for="customize">customize</label>
           </div>
         </div>
         <div class="spacer" />
-        <RouterLink to="/about" style="display: inline-flex; align-items: center; float: right;">test</RouterLink>
+        <RouterLink
+          to="/about"
+          style="display: inline-flex; align-items: center; float: right"
+        >
+          test
+        </RouterLink>
       </div>
       <div v-if="customize">
         <div class="row mtb025">
-          <input type="checkbox" name="addDigit" id="addDigit" v-model="digit" @change="generate">
+          <input
+            type="checkbox"
+            name="addDigit"
+            id="addDigit"
+            v-model="digit"
+            @change="generate"
+          />
           <label for="addDigit">add digit</label>
         </div>
         <div class="row mtb025">
-          <input type="checkbox" name="keepCase" id="keepCase" v-model="keepCase" @change="generate">
+          <input
+            type="checkbox"
+            name="keepCase"
+            id="keepCase"
+            v-model="keepCase"
+            @change="generate"
+          />
           <label for="keepCase">keep case</label>
         </div>
         <div class="row mtb025" v-if="!keepCase">
-          <input type="checkbox" name="uppercase" id="uppercase" v-model="uppercase" @change="generate">
+          <input
+            type="checkbox"
+            name="uppercase"
+            id="uppercase"
+            v-model="uppercase"
+            @change="generate"
+          />
           <label for="uppercase">first letter uppercase</label>
         </div>
         <div class="row mtb025">
-          <input type="checkbox" name="addDash" id="addDash" v-model="dash" @change="generate">
+          <input
+            type="checkbox"
+            name="addDash"
+            id="addDash"
+            v-model="dash"
+            @change="generate"
+          />
           <label for="addDash">add dashes</label>
         </div>
       </div>
