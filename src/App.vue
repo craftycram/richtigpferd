dd88032d23f0a99ce40abf6d3521ead8003f2446
Marc Rufeis <github@craftycram.net>
2024-09-27 17:12:13 +0200

    add seo meta

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,17 @@
+<script setup>
+import { useHead } from '@unhead/vue'
+
+useHead({
+  title: 'r1cht1g pf3rd',
+  meta: [
+    {
+      name: 'description',
+      content: 'A password generator that generates passwords that are really secure and easy to remember.',
+    },
+  ],
+})
+</script>
+
 <template>
   <RouterView />
 </template>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,10 @@ import router from './router';
 
 import i18n from '@/plugins/i18n';
 
+import { createHead } from 'unhead'
+
+const head = createHead()
+
 const app = createApp(App);
 
-app.use(router).use(i18n).mount('#app');
+app.use(router).use(i18n).use(head).mount('#app');
