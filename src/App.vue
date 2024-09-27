05d418b77a5fd1917f2c74f7dcb568384219147e
Marc Rufeis <github@craftycram.net>
2024-09-27 17:19:20 +0200

    change seo meta

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,11 @@
 import { useHead } from '@unhead/vue'
 
 useHead({
-  title: 'r1cht1g pf3rd',
+  title: 'r1cht1g pf3rd – generate secure and easy to remember passwords',
   meta: [
     {
       name: 'description',
-      content: 'A password generator that generates passwords that are really secure and easy to remember.',
+      content: 'A password generator that generates passwords that are really secure and easy to remember. Using a simple algorithm, the passwords are generated based on a word that you can remember.',
     },
   ],
 })
