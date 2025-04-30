6a009922503331acbe7919519d5b94fb995eb66a
Marc Rufeis <github@craftycram.net>
2025-04-30 19:01:20 +0200

    fix for vercel

--- server/utils/wordlist.ts
+++ server/utils/wordlist.ts
@@ -2,7 +2,7 @@ import { readFileSync } from 'node:fs';
 import { join } from 'node:path';
 
 // Read wordlist during build time
-const wordlistPath = join(process.cwd(), 'public', 'wordlist.txt');
+const wordlistPath = join(process.cwd(), 'wordlist.txt');
 const wordlistContent = readFileSync(wordlistPath, 'utf8');
 
 export default wordlistContent;
